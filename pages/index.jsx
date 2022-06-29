4afe2f1d3b6014e36184154d9a9de1523467f93e
LuteJr <101115777+LuteJr@users.noreply.github.com>
2022-06-29 11:13:25 +0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        <i className="fa-solid fa-sliders"></i>Hello world!
+      </h1>
+    </div>
+  );
+}
