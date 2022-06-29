db1ec50cf862dc7d562fe7cbd006f9dc7bcf2413
LuteJr <101115777+LuteJr@users.noreply.github.com>
2022-06-29 14:01:08 +0300

    fonts, fontawesome & bootstrap test t2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b6da471bed5ee351"; }
+/******/ 		__webpack_require__.h = function() { return "e9aa4e99d10eaebd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,9 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <div className="container m-5 text-center">
       <i className="fa-solid fa-sliders"></i>
-      <h1>New Stuff</h1>
+      <h1>New Stuff - testing t1</h1>
     </div>
   );
 }
